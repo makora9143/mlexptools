15aa46971287de007dcb13d77d6aa3b9f242d0b7
Makoto Kawano <makoto.kawano@gmail.com>
2024-02-19 15:28:15 +0900

    Fix Mean for MPS

--- mlexptools/__init__.py
+++ mlexptools/__init__.py
@@ -1,8 +1,12 @@
 from mlexptools.writer import MlflowWriter
 from mlexptools import metrics
+from mlexptools.utils import get_logger
+from torcheval import metrics as tm
 
+tm.Mean = metrics.MeanMetric
 
 __all__ = [
     'metrics',
     'MlflowWriter',
+    'get_logger',
 ]
--- mlexptools/metrics/mean_metric.py
+++ mlexptools/metrics/mean_metric.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, TypeVar
+from typing import Optional, TypeVar
 
 from torcheval.metrics import Mean
 import torch
@@ -22,7 +22,8 @@ class MeanMetric(Mean):
         *,
         device: Optional[torch.device] = None,
     ) -> None:
-        super().__init__(device=device)
+        super().__init__()
+        self._device = device
         # weighted sum of values over the entire state
         self._add_state(
             "weighted_sum", torch.tensor(0.0, device=self.device, dtype=torch.float32 if self.device == torch.device('mps') else torch.float64)
@@ -31,34 +32,3 @@ class MeanMetric(Mean):
         self._add_state(
             "weights", torch.tensor(0.0, device=self.device, dtype=torch.float32 if self.device == torch.device('mps') else torch.float64)
         )
-
-    @torch.inference_mode()
-    # pyre-ignore[14]: inconsistent override on *_:Any, **__:Any
-    def update(
-        self: TMean,
-        input: torch.Tensor,
-        *,
-        weight: Union[float, int, torch.Tensor] = 1.0,
-    ) -> TMean:
-        """
-        Compute weighted mean. When weight is not provided, it calculates the unweighted mean.
-
-        weighted_mean = sum(weight * input) / sum(weight)
-
-        Args:
-            input (Tensor): Tensor of input values.
-            weight(optional): Float or Int or Tensor of input weights. It is default to 1.0. If weight is a Tensor, its size should match the input tensor size.
-        Raises:
-            ValueError: If value of weight is neither a ``float`` nor a ``int'' nor a ``torch.Tensor`` that matches the input tensor size.
-        """
-        if input.device != self.device:
-            _logger.warning(
-                f"input tensor is on {input.device} while the metric is on {self.device}."
-                "Moving the metric to the device of the input tensor."
-            )
-            self.weighted_sum = self.weighted_sum.to(input)
-            self.weights = self.weights.to(input)
-
-        super().update(input, weight=weight)
-
-
